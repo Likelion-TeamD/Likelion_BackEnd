360d88e2b37e0a86336e266311d28268d00bed33
sunj0414 <sunj0414@naver.com>
2023-08-14 16:44:19 +0900

    방명록_재정비_commet모델_방명록으로_url도_다시

--- farmin/urls.py
+++ farmin/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'farmin'
 
 urlpatterns = [
-    path('posts/', PostViewSet.as_view({'post':'create','get':'list'})),
-    path('<int:pk>/', PostViewSet.as_view({'delete':'destroy', 'patch':'update'})),
-    path('<int:pk>/comment/', CommentViewSet.as_view({'post':'create','get':'list'})), #댓글달기 기능은 따로 필요하지 않은 것 같은데?(feat. 피그마디자인)
-    path('<int:pk>/like', PostLikeViewset.as_view({'post':'create'}), name = 'like'),
+    path('comment/', GuestbookViewSet.as_view({'post':'create','get':'list'})),
+    path('<int:pk>/', GuestbookViewSet.as_view({'delete':'destroy', 'patch':'update'})),
+    # path('<int:pk>/comment/', CommentViewSet.as_view({'post':'create','get':'list'})), #댓글달기 기능은 따로 필요하지 않은 것 같은데?(feat. 피그마디자인)
+    # path('<int:pk>/like', PostLikeViewset.as_view({'post':'create'}), name = 'like'),
     
     path('', views.index, name = 'index'),
-    path('<int:post_id>/', views.detail, name = 'detail'),
+    path('<int:guestbook_id>/', views.detail, name = 'detail'),
 ]
