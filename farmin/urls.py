16d45a85386e435f857c6cd54d21a569b27ca0ef
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2023-08-17 02:01:11 +0900

    post데이터 추가, post model 변경

--- farmin/urls.py
+++ farmin/urls.py
@@ -6,20 +6,16 @@ from . import views
 app_name = 'farmin'
 
 urlpatterns = [
-    #농부마다 다른 홈페이지 만들거라서 farmer_id 받아와서 작업
+    #농부마다 다른 정보
     path('<int:farmer_id>/', farmer_page, name='farmer_page'),
-    #농부마다 다른 농장 url
+    #농부마다 다른 농장 사진
     path('<int:farmer_id>/farm/', views.farm_page, name='farm_page'),
     #각 농부마다 다른 판매페이지
     path('<int:farmer_id>/sale/', views.PostViewSet.as_view({'post':'create', 'get':'list'}),name='sale_page'),
-    path('<int:farmer_id>/sale/<int:pk>/', views.PostViewSet.as_view({'delete':'destroy'})),
+    path('<int:farmer_id>/sale/<int:pk>/', views.PostViewSet.as_view({'delete':'destroy'}), name = 'delete'),
     path('<int:farmer_id>/sale/<int:pk>/like/', PostLikeViewset.as_view({'post':'create'}), name = 'like'),
      #각 농부마다 다른 방명록페이지
     path('<int:farmer_id>/guestbook/', views.GuestbookViewSet.as_view({'post': 'create', 'get': 'list'}), name='guestbook_page'),
     path('<int:farmer_id>/guestbook/<int:pk>/', views.GuestbookViewSet.as_view({'delete':'destroy'}), name='delete_guestbook'),
-    #test
-    path('<int:farmer_id>/test/',views.test,name='test'),
-    #
-    path('', views.index, name='index'),
     # path('detail/<int:guestbook_id>/', views.detail, name='detail'),
 ]
