786a92c770edc6c4dea8468c5e90917f884111c5
JUNGEUN <122519994+jsilver01@users.noreply.github.com>
2023-08-17 11:25:10 +0900

    방명록데이터추가

--- farmin/urls.py
+++ farmin/urls.py
@@ -14,7 +14,9 @@ urlpatterns = [
     path('<int:farmer_id>/sale/', views.PostViewSet.as_view({'post':'create', 'get':'list'}),name='sale_page'),
     path('<int:farmer_id>/sale/<int:pk>/', views.PostViewSet.as_view({'delete':'destroy'}), name = 'delete'),
     path('<int:farmer_id>/sale/<int:pk>/like/', PostLikeViewset.as_view({'post':'create'}), name = 'like'),
-     #각 농부마다 다른 방명록페이지
+    #좋아요많은 순으로 2개 정렬
+
+    #각 농부마다 다른 방명록페이지
     path('<int:farmer_id>/guestbook/', views.GuestbookViewSet.as_view({'post': 'create', 'get': 'list'}), name='guestbook_page'),
     path('<int:farmer_id>/guestbook/<int:pk>/', views.GuestbookViewSet.as_view({'delete':'destroy'}), name='delete_guestbook'),
     # path('detail/<int:guestbook_id>/', views.detail, name='detail'),
